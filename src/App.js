ee1bac23b6a89a38f89571363fe8fcc640ab94e6
mahershmesa <mahershmesa@gmail.com>
2023-12-09 17:39:35 +0300

    Modify project files and format objects

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import { Header, Footer, Container,Hero } from './components/index'
+
+const App = ()=> {
+    return (
+        <>
+          <Header/>
+          <Container> 
+            <Hero/>
+          </Container>
+         {/* <Footer/> */}    
+        </>
+        
+    )
+}
+
+export default App
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,19 @@
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero-main'>
+        <div className='hero-text'>
+            <h6 className='hero-subtitle'>Welcom to cybrog</h6>
+            <h4 className='hero-title'><em>Browse</em>our popular games here</h4>
+            <div className="main-button">
+                <a href='browse.html'>Browse Now</a>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Hero
