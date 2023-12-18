f8fb0fa6de3e97181aa6a780ae560774d06d2c5c
mahershmesa <mahershmesa@gmail.com>
2023-12-18 04:45:18 +0300

    Format all items in mostpopular

--- src/sections/MostPopular/MostPopular.js
+++ src/sections/MostPopular/MostPopular.js
@@ -11,7 +11,101 @@ const MostPopular = () => {
        <div className='most-popular-items'>
          <div className='most-popular-item'>
             <div className='card-wrapper'>
-               <img src={popular_01} />
+               <img className='most-popular-item-image' src={popular_01} />
+               <div className='most-popular-item-content'>
+                <h4 className='most-popular-item-title'>
+                    Fortnite<br />
+                    <span>Sandbox</span>
+                    </h4> 
+                    <ul>
+                        <li><span>4.8</span></li>
+                        <li><span>2.3M</span></li>
+                    </ul>
+               </div>
+            </div>
+
+         </div>
+         <div className='most-popular-item'>
+            <div className='card-wrapper'>
+               <img className='most-popular-item-image' src={popular_01} />
+               <div className='most-popular-item-content'>
+                <h4 className='most-popular-item-title'>
+                    Fortnite<br />
+                    <span>Sandbox</span>
+                    </h4> 
+                    <ul>
+                        <li><span>4.8</span></li>
+                        <li><span>2.3M</span></li>
+                    </ul>
+               </div>
+            </div>
+
+         </div>
+
+         <div className='most-popular-item'>
+            <div className='card-wrapper'>
+               <img className='most-popular-item-image' src={popular_01} />
+               <div className='most-popular-item-content'>
+                <h4 className='most-popular-item-title'>
+                    Fortnite<br />
+                    <span>Sandbox</span>
+                    </h4> 
+                    <ul>
+                        <li><span>4.8</span></li>
+                        <li><span>2.3M</span></li>
+                    </ul>
+               </div>
+            </div>
+
+         </div>
+
+         <div className='most-popular-item'>
+            <div className='card-wrapper'>
+               <img className='most-popular-item-image' src={popular_01} />
+               <div className='most-popular-item-content'>
+                <h4 className='most-popular-item-title'>
+                    Fortnite<br />
+                    <span>Sandbox</span>
+                    </h4> 
+                    <ul>
+                        <li><span>4.8</span></li>
+                        <li><span>2.3M</span></li>
+                    </ul>
+               </div>
+            </div>
+
+         </div>
+
+         <div className='most-popular-item'>
+            <div className='card-wrapper'>
+               <img className='most-popular-item-image' src={popular_01} />
+               <div className='most-popular-item-content'>
+                <h4 className='most-popular-item-title'>
+                    Fortnite<br />
+                    <span>Sandbox</span>
+                    </h4> 
+                    <ul>
+                        <li><span>4.8</span></li>
+                        <li><span>2.3M</span></li>
+                    </ul>
+               </div>
+            </div>
+
+         </div>
+
+         <div className='most-popular-item'>
+            <div className='card-wrapper'>
+               <img className='most-popular-item-image' src={popular_01} />
+               <div className='most-popular-item-content'>
+                <h4 className='most-popular-item-title'>
+                    Fortnite<br />
+                    <span>Sandbox</span>
+                    </h4> 
+                    <ul>
+                        <li><span>4.8</span></li>
+                        <li><span>2.3M</span></li>
+                    </ul>
+               </div>
             </div>
 
          </div>
