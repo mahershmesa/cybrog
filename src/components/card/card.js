4d4393282d208dd25e646e3311d7cf38fc40c23d
mahershmesa <mahershmesa@gmail.com>
2023-12-20 02:49:46 +0300

    new component and style the var

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,21 +1,22 @@
 import React from 'react'
+import {FaStar , FaDownload} from 'react-icons/fa'
+import {BsFillBrightnessHighFill} from 'react-icons/bs'
 
-import popular_01 from '../../assets/images/popular-01.jpg'
 
 
 const card = (props) => {
   return (
     <div className='most-popular-item'>
             <div className='card-wrapper'>
-               <img className='most-popular-item-image' src={popular_01} />
+               <img className='most-popular-item-image' src={props.image} />
                <div className='most-popular-item-content'>
                 <h4 className='most-popular-item-title'>
                     {props.title}<br />
                     <span>{props.category}</span>
                     </h4> 
                     <ul>
-                        <li><span>{props.rate}</span></li>
-                        <li><span>{props.download }</span></li>
+                        <li><span style={{"color":"var(--color-icons)"}}><BsFillBrightnessHighFill/></span> <span>{props.rate}</span></li>
+                        <li><span style={{"color":"var(--color-primary)"}}><FaDownload/></span> <span>{props.download }</span></li>
                     </ul>
                </div>
             </div>
