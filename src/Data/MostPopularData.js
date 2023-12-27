80fb8187070eedb55ce0d2a1f9bf3ed4d68bfec3
mahershmesa <mahershmesa@gmail.com>
2023-12-27 05:02:16 +0300

    update files

---
+++ src/Data/MostPopularData.js
@@ -0,0 +1,13 @@
+const mostPopularItems = [
+
+    { id : "most_popular_item_0", image: popular_01, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
+    { id : "most_popular_item_1", image: popular_01, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
+    { id : "most_popular_item_2", image: popular_01, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
+    { id : "most_popular_item_3", image: popular_01, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
+    { id : "most_popular_item_4", image: popular_01, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
+    { id : "most_popular_item_5", image: popular_01, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
+    { id : "most_popular_item_6", image: popular_01, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
+    { id : "most_popular_item_7", image: popular_01, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
+
+
+]
