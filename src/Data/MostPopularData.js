8347955f278468fdcea0d0c901d7afaedcc778ea
mahershmesa <mahershmesa@gmail.com>
2023-12-28 05:14:57 +0300

    Managing data through the matrix

--- src/Data/MostPopularData.js
+++ src/Data/MostPopularData.js
@@ -1,13 +1,22 @@
-const mostPopularItems = [
+import popular_01 from '../assets/images/popular-01.jpg'
+
+import popular_02 from '../assets/images/popular-02.jpg'
+import popular_03 from '../assets/images/popular-03.jpg'
+import popular_04 from '../assets/images/popular-04.jpg'
+
+
+const mostPopularData = [
 
     { id : "most_popular_item_0", image: popular_01, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
-    { id : "most_popular_item_1", image: popular_01, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
-    { id : "most_popular_item_2", image: popular_01, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
-    { id : "most_popular_item_3", image: popular_01, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
-    { id : "most_popular_item_4", image: popular_01, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
-    { id : "most_popular_item_5", image: popular_01, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
-    { id : "most_popular_item_6", image: popular_01, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
-    { id : "most_popular_item_7", image: popular_01, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
+    { id : "most_popular_item_1", image: popular_02, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
+    { id : "most_popular_item_2", image: popular_03, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
+    { id : "most_popular_item_3", image: popular_04, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
+    { id : "most_popular_item_4", image: popular_04, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
+    { id : "most_popular_item_5", image: popular_03, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
+    { id : "most_popular_item_6", image: popular_02, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
+    { id : "most_popular_item_7", image: popular_02, title: "Frontair", category: "Sandbox", rate:"3.8",download: "2.3m"},
 
 
 ]
+
+export default mostPopularData
--- src/sections/MostPopular/MostPopular.js
+++ src/sections/MostPopular/MostPopular.js
@@ -1,31 +1,21 @@
 import './MostPopular.css'
 import { Card , SectionHeader , SectionWrapper } from '../../components/index'
-import popular_01 from '../../assets/images/popular-01.jpg'
 
-import popular_02 from '../../assets/images/popular-02.jpg'
-import popular_03 from '../../assets/images/popular-03.jpg'
-import popular_04 from '../../assets/images/popular-04.jpg'
 
+import mostPopularData from '../../Data/MostPopularData'
 
  
 const MostPopular = () => {
+
+   const cards = mostPopularData.map(card => {
+    return <Card key={card.id}  image={card.image}  title={card.title} category={card.category} rate={card.rate} download={card.download}/>
+   })
   return (
     <>
     <SectionWrapper>
       <SectionHeader>MostPopular</SectionHeader>
        <div className='most-popular-items'>
-        
-        <Card image={popular_01} title="fortnite" category="sandbox" rate="3.8" download="2.3M" />
-        <Card image={popular_02} title="pubg" category="stream-x" rate="4.8" download="1.1M"/>
-        <Card image={popular_03} title="dota2" category="legendary" rate="2.7" download="5.3M"/>
-        <Card image={popular_04} title="cs-go" category="Battle 5" rate="3.9" download="6.1M" />
-        <Card image={popular_01} title="fortnite" category="sandbox" rate="3.8" download="2.3M" />
-        <Card image={popular_02} title="pubg" category="stream-x" rate="4.8" download="1.1M"/>
-        <Card image={popular_03} title="dota2" category="legendary" rate="2.7" download="5.3M"/>
-        <Card image={popular_04} title="cs-go" category="Battle 5" rate="3.9" download="6.1M" />
-
-
-        
+        {cards}
        </div>
        </SectionWrapper>
     </>
